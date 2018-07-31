97c3900b6e5fa4f589c58ef20d1d9e6f08c9d8cd
Jvanuk <40635898+Jvanuk@users.noreply.github.com>
2018-07-31 16:13:14 +0300

    Update anket.js

--- src/anket.js
+++ src/anket.js
@@ -1,21 +1,21 @@
-var year = prompt('Сколько вам лет?');
-  if (year < 18)
-    {
-      alert ('Bye Kid');
-      throw "stop"
-    }
-var name = prompt('Введите ваше имя');
-if (name == '')
-    {
-    alert('Вeдите имя правильно!');
-    }
-    else
-      {
-        var anket = confirm('Добрый день, ' + name + ' хотите добавить еще информацию о себе?');
-        if (anket == true )
+    var year = prompt('Сколько вам лет?');
+      if (year < 18)
+        {
+          alert ('Bye Kid');
+          throw "stop"
+        }
+    var name = prompt('Введите ваше имя');
+    if (name == '')
+        {
+        alert('Вeдите имя правильно!');
+        }
+        else
           {
-            var mulo = prompt ('Введите ваш имейл')
-            alert('Поздравляем ' + name + ' Вам ' + year + ' лет и ваш имейл ' + mulo)
-          }
+            var info = confirm('Добрый день, ' + name + ' хотите добавить еще информацию о себе?');
+            if (info == true )
+              {
+                var email = prompt ('Введите ваш имейл')
+                alert('Поздравляем ' + name + ' Вам ' + year + ' лет и ваш имейл ' + email)
+              }
 else alert('Поздравляем ' + name + ' Вам ' + year + ' лет!')
-    }
+        }
